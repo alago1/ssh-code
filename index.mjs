d229126bb7a3f9fd773631ea5e150b5ae01c680a
Allan Lago <allanlago1@gmail.com>
2024-03-30 02:29:00 -0700

    resolve user, host, path when possible

--- index.mjs
+++ index.mjs
@@ -3,11 +3,35 @@
 import { spawn } from "child_process";
 import { Argument, Command } from "commander";
 import { readFile } from "fs/promises";
+import { existsSync } from "fs";
+import { resolve } from "path";
+import os from "os";
 const { version } = JSON.parse(
   await readFile(new URL("./package.json", import.meta.url))
 );
 
 const program = new Command("ssh-code");
+
+const resolveUserHostPath = (target) => {
+  const resolvedPath = resolve(target);
+  if (existsSync(resolvedPath)) {
+    return {
+      user: os.userInfo().username,
+      host: os.hostname(),
+      path: resolvedPath,
+    };
+  }
+
+  const targetRegex = /^(((?<user>[^@]+)@)?(?<host>[^:]+):)?(?<path>.+)$/;
+  const result = targetRegex.exec(target);
+
+  if (!result) {
+    console.error("Invalid target");
+    process.exit(1);
+  }
+
+  return result.groups;
+};
 
 program
   .version(version)
@@ -20,14 +44,7 @@ program
   .action((target) => {
     // Regex for the target string
     // The user is optional
-    const targetRegex = /^((?<user>[^@]+)@)?(?<host>[^:]+):(?<path>.+)$/;
-    const result = targetRegex.exec(target);
-
-    if (!result) {
-      console.error("Invalid target");
-      process.exit(1);
-    }
-    const { user, host, path } = result.groups;
+    const { user, host, path } = resolveUserHostPath(target);
 
     // Run vscode with the ssh folder
     const connection = `${user ? user + "@" : ""}${host}`;
